71f2af6d0ed1090e0993428c2e18b97a3054858f
Taku <marowatk@gmail.com>
2022-04-11 19:43:40 +0200

    Implemented Basic Gameplay
    The character can now move, and the basic level is set up

---
+++ Assets/_Project/Scipts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rigidBody;
+    private float horizontalInput;
+    public float horizontalMultiply = 2f;
+
+    public float xBoundary;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+
+       
+        Vector3 forwardMovement = transform.forward * moveSpeed * Time.fixedDeltaTime;
+        Vector3 horizontalMovement = transform.right * horizontalInput * moveSpeed * Time.fixedDeltaTime * horizontalMultiply;
+        rigidBody.MovePosition(rigidBody.position + forwardMovement + horizontalMovement);
+
+        
+    }
+}
